0b9af451a4f984824f94125a1d94ae5584dc8494
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-16 11:54:00 +0400

    task

---
+++ usernameandpassword.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string username = "";
+            string password;
+            User user = new User(username);
+            do
+            {
+                Console.Write("Istifadeci adini daxil edin: ");
+                username = Console.ReadLine();
+                user.Username = username;
+                if (user.Username == null)
+                    Console.WriteLine("Istifadeci adi en azi 6,en coxu 25 simboldan ibaret ola biler  ");
+            } while (user.Username == null);
+            do
+            {
+                Console.Write("Sifreni daxil edin: ");
+                password = Console.ReadLine();
+                user.Password = password;
+                if (user.Password == null)
+                    Console.WriteLine("Sifre en azi 8,en coxu 25 simboldan ibaret ola biler  ");
+            } while (user.Password == null);
+
+
+        }
+    }
+}
